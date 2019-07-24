9a1a07d25dd98178fb534bc21daec44d4a36aff5
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-07-24 13:05:02 -0700

    working on form.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
+import Form from './components/Form.js'
+import TeamMember from './components/TeamMembers.js'
+import teamData from './components/teamData.js'
+
+
 function App() {
+
+  const [teamMember, setTeamMember]= useState(teamData)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TeamMember />
+      <Form />
     </div>
   );
 }
---
+++ src/components/teamData.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const teamData ={
+    "member-1": {"name": "Hannah Tuttle",
+    "email": "hannah@gmail.com",
+    "role": "front-end Developer"},
+    "member-2": {"name": "Hannah Tuttle",
+    "email": "hannah@gmail.com",
+    "role": "front-end Developer"}
+
+}
+
+export default teamData
